28a7cd13c1fbe908dcd9b586b106cd0b73d91abf
Max <maksimus.pugachev@gmail.com>
2023-03-23 19:23:06 +0500

    MVP with auth

--- Airtable/api-airtable.js
+++ Airtable/api-airtable.js
@@ -11,17 +11,20 @@ const baseID = process.env.AIRTABLE_BASE_ID;
 
 
 
-export async function GETDATA (tableName, maxRecords){
-    
-    var apiUrl = "https://api.airtable.com/v0/" + baseID + "/" + tableName+"?maxRecords="+maxRecords;
-    
-    var resp = await fetch(apiUrl, {
-        method: 'GET',
-        headers: {
-            Authorization: "Bearer " + airtable_key
-          }
-    }).then(resp => resp.json())
+export async function GETDATA (tableName, maxRecords, authFlag){
+    if(authFlag==true){
+        var apiUrl = "https://api.airtable.com/v0/" + baseID + "/" + tableName+"?maxRecords="+maxRecords;
 
-    return resp;
-    
+        var resp = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                Authorization: "Bearer " + airtable_key
+                }
+        }).then(resp => resp.json())
+
+        return resp;
+    } else {
+        let errorAuth = "You do not have access to the data in this table"
+        return errorAuth;
+    }
 }
--- index.js
+++ index.js
@@ -1,17 +1,88 @@
 import express from "express";
 import Airtable from "airtable";
 import { GETDATA } from "./Airtable/api-airtable.js";
+import fs from "fs";
 
 const port = 8000;
 
 const app = express();
+//Флаг авторизации
+var hashForAuth = null;
 
 app.get('/get/data', async (req,res)=>{
-    //console.log(req.body)
+    
+    //Парсинг параметров запроса
     let table = req.query.table;
     let maxRecords = req.query.maxrecords;
-    let airtableres = await GETDATA(table, maxRecords);
+    let auth = req.query.auth;
+
+    let flagForAuth = false;
+    
+    //считывание данных авторизации
+    hashForAuth = fs.readFileSync("auth-key.json");
+    hashForAuth = JSON.parse(hashForAuth);
+
+    
+
+    //Авторизация
+    switch(table){
+        case "block":{
+            if(auth == hashForAuth.block){
+                flagForAuth = true;
+            }
+        }break;
+        case "course":{
+            if(auth == hashForAuth.course){
+                flagForAuth = true;
+            }
+        }break;
+        case "author":{
+            if(auth == hashForAuth.author){
+                flagForAuth = true;
+            }
+        }break;
+        case "promocode":{
+            if(auth == hashForAuth.promocode){
+                flagForAuth = true;
+            }
+        }break;
+        case "acts":{
+            if(auth == hashForAuth.acts){
+                flagForAuth = true;
+            }
+        }break;
+        case "student":{
+            if(auth == hashForAuth.student){
+                flagForAuth = true;
+            }
+        }break;
+        case "pay":{
+            if(auth == hashForAuth.pay){
+                flagForAuth = true;
+            }
+        }break;
+        case "advertising":{
+            if(auth == hashForAuth.advertising){
+                flagForAuth = true;
+            }
+        }break;
+        case "idea":{
+            if(auth == hashForAuth.idea){
+                flagForAuth = true;
+            }
+        }break;
+        case "lessions":{
+            if(auth == hashForAuth.lessions){
+                flagForAuth = true;
+            }
+        }break;
+        
+    }
+    //Запрос на Airtable
+    let airtableres = await GETDATA(table, maxRecords, flagForAuth);
+    //Отправка данных
     res.status(200).json(airtableres)
+    
     //console.log(airtableres)
 })
 
