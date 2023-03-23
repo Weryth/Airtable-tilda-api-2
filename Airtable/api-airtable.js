5e6494966477c616e19c6ed7ebcce4cd3bdfc532
Max <maksimus.pugachev@gmail.com>
2023-03-23 14:56:11 +0500

    Server working

---
+++ Airtable/api-airtable.js
@@ -0,0 +1,8 @@
+import Airtable from "airtable";
+import * as dotenv from 'dotenv';
+
+const airtable_key = process.env.AIRTABLE_API_KEY;
+
+function GETDATA (){
+
+}
--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const express = require('express');
+import express from "express";
+import Airtable from "airtable";
+
+const port = 8000;
+
 const app = express();
+
+app.post('/hello', (req,res)=>{
+    res.status(200).json({body: "Привет!"})
+    
+})
+
+app.listen(port, ()=>{
+    console.log(`Server has been starten on http://localhost:${port}`);
+})
 
